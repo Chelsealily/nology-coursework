da9c3f020ac98986689d10c9f7294e255b4ed68a
Chelsea <cpearl_li@live.com>
2023-11-01 18:16:58 +0000

    Created 2 files, French translater + retirement

---
+++ development/javascript/script.js
@@ -0,0 +1,19 @@
+function calculateYearsUntilRetirement(event) {
+    const currentAge = event.target.value;
+
+    const yearsLeft = 65 - currentAge;
+    alert(yearsLeft)
+}
+
+function getGreetingInFrench(event) {
+    const greeting = event.target.value;
+
+if (greeting == "Hello") {
+    alert ("Bonjour")
+} else if (greeting == "Goodbye") {
+    alert ('Au Revoir')
+} else {
+    alert ('not recognised')
+}
+
+}
