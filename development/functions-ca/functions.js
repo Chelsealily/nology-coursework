a8ab9f666a7b0ab3ebb3128f27a16c8aca932cd4
Chelsea <cpearl_li@live.com>
2023-11-06 15:50:43 +0000

    function practies

---
+++ development/functions-ca/functions.js
@@ -0,0 +1,9 @@
+// ** Challenge ** 
+
+// write a function to find the peremeter of a rectangle
+
+const calculatePerimeter = (width, height) => {
+    return width + width + height + height;
+}
+
+console.log (calculatePerimeter(1,1));
