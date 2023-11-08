4cd70c7a0a4adc3bd30d887848bcd8e227f9a69b
Chelsea <cpearl_li@live.com>
2023-11-08 12:29:23 +0000

    added header and devicemockup img

--- development/functions-ca/functions-challenge.js
+++ development/functions-ca/functions-challenge.js
@@ -10,3 +10,49 @@ function isNumberPositive(num) {
 
 isNumberPositive(-1);
 isNumberPositive(10); 
+
+
+// write function that takes a number of days and converts into age
+
+const convertDaysToAge = (days) => {
+    const daysInYears = (days / 365);
+    return daysInYears
+}
+
+console.log (convertDaysToAge(3650));
+console.log (convertDaysToAge(6570));
+
+
+// write a function that takes 3 numbers and returns the largest of the 3
+
+const getLargestNumber = (num1, num2, num3) => {
+    console.log (Math.max(num1,num2,num3));
+}
+
+getLargestNumber(2,1,4);
+getLargestNumber(6,2,3);
+
+// write a function that takes an array of names and returns the last name from the arrays
+// getLastName([”Charlie”, “Rob”, “Andy”]); // returns “Andy”
+// getLastName(["Ash","Stu"]); // returns "Stu"
+
+let getLastName = ["Charlie", "Rob", "Andy"];
+    let lastItem = getLastName[getLastName.length - 1];
+console.log (lastItem);
+
+
+// write a function that takes an array of numbers and returns true if all numbers are pos but false if one of more numbers are negative in the array.
+// allNumbersPositive([2,4,5]); // returns true
+// allNumbersPositive([-5,4,6]); // returns false
+
+function allNumbersPositive (v1,v2,v3) {
+
+if (Math.sign(v1,v2,v3) === 1) {
+    console.log (true);
+} else if (Math.sign(v1,v2,v3) === -1) {
+    console.log (false);
+}    
+} 
+
+allNumbersPositive(2,4,5);
+allNumbersPositive(-5,4,6);
--- development/functions-ca/functions.js
+++ development/functions-ca/functions.js
@@ -125,15 +125,3 @@ for (let index=0; index < names.length; index++) {
     console.log(names[index]);
 }
 
-// create function that shows true if pos and false if neg
-
-function isNumberPositive(num) {
-    if (Math.sign(num) === 1) {
-        console.log (true);
-    } else if (Math.sign(num) === -1) {
-        console.log (false);
-    }    
-} 
-
-isNumberPositive(-1);
-isNumberPositive(10); 
