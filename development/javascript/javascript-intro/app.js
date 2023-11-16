2542bb5eecbf621e5c6644d742e5318b3420902c
Chelsea <cpearl_li@live.com>
2023-11-16 10:08:44 +0000

    renamed old index

--- development/javascript/javascript-intro/app.js
+++ development/javascript/javascript-intro/app.js
@@ -1,4 +1,20 @@
 
+//variables and data types
+
 const coach = "Ollie"
 const isBest = true
 let score = 50
+
+//if statement - if coach = ollie then console.log ollie is a coach
+
+if (coach === "Ollie") {
+    console.log ("Ollie is a coach")
+    console.log (`${coach} is the coach`)
+    console.log (coach + " is a coach")
+}
+
+if (score===30) {
+    console.log (`the score is ${score}`)
+    } else {
+        console.log (`score is actually ${score}`)
+    }
