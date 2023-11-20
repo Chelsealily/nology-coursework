03facd5290630941d9b2b626050e296e5ff09f0c
Chelsea <cpearl_li@live.com>
2023-11-20 17:22:48 +0000

    minor changes to port-strugglign on grid

--- development/javascript/javascript-intro/arrays.js
+++ development/javascript/javascript-intro/arrays.js
@@ -1,4 +1,4 @@
-const coaches = ["Ollie", "Charlie", "Rob", "Sam"]
+/* const coaches = ["Ollie", "Charlie", "Rob", "Sam"]
 
 console.log(coaches[2]);
 console.log(coaches[0]);
@@ -18,3 +18,42 @@ console.log(coaches)
 
 coaches.unshift("Hei")
 console.log(coaches)
+
+ for (let index=0; index<6; index++) {
+    console.log("the index is " + index)
+ } 
+
+const coaches = ["Dan", "Jake", "Josie"];
+
+for (let index=0; index <coaches.length; index++) {
+    const coach = coaches [index];
+    console.log("Hey there " + coach + " wave")
+}
+
+*/
+
+// - Create a script that will use a loop to 
+// iterate through an array of scores (numbers) 
+//and log to the console "12 is a big score!" 
+//or "50 is a big score!" for each score 
+//that is greater than **10**.
+
+ 
+
+ const numbers = [8, 9, 12, 50, 67, 433];
+
+ /*for (let i=0; i < myScores.length; i++) {
+    if (myScores[i] > 10) {
+        console.log (`${myScores[i]} is a big score`)
+    }
+ }*/
+
+ const printBigScore = (myScores, threshold) => {
+    for(let index=0; index < myScores.length; index++) {
+        if (myScores[index] > threshold) {
+            console.log(`${myScores[index]} is a big number`)
+        }
+    }
+ }
+
+ printBigScore (numbers,50)
